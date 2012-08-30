6da02c369468af36f84cf306e20a46c4b52697c7
David Lynch <kemayo@gmail.com>
2012-08-30 14:40:25 -0500

    I made these changes so long ago that I can't remember why, but hey, why not

--- addon.lua
+++ addon.lua
@@ -1,6 +1,8 @@
 local myname, ns = ...
 local myfullname = GetAddOnMetadata(myname, "Title")
 
+ServerRestart = ns
+
 -- debug
 local debugf = tekDebug and tekDebug:GetFrame(myname)
 function ns.Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
@@ -14,19 +16,19 @@ function ns:UnregisterEvent(...) for i=1,select("#", ...) do f:UnregisterEvent((
 
 -- Actual functionality
 --8/2 08:03:29.127  [SERVER] This realm is scheduled for a rolling restart. Thank you for your patience and understanding.
---8/2 08:03:35.504  [SERVER] Restart in 15:00
---8/2 08:03:35.504  [SERVER] Shutdown in 15:00
-function addon:CHAT_MSG_SYSTEM(event, msg)
+--8/2 08:03:35.504  [SERVER] Restart in 10:00
+--8/2 08:03:35.504  [SERVER] Shutdown in 10:00
+function ns:CHAT_MSG_SYSTEM(event, msg)
 	if not msg:find("[SERVER]") then return end
 	if msg:find("Shutdown") or (msg:find("realm") and (msg:find("restart") or msg:find("maintenance"))) then
 		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSDStart01.wav", "Master")
-	elseif msg:find("10:00") then
+	elseif msg:find(" 10:00") then
 		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD10.wav", "Master")
-	elseif msg:find("5:00") then
+	elseif msg:find(" 5:00") then
 		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD05.wav", "Master")
-	elseif msg:find("1:00") then
+	elseif msg:find(" 1:00") then
 		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD01.wav", "Master")
-	elseif msg:find("0:15") then
+	elseif msg:find(" 0:15") then
 		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD00.wav", "Master")
 	end
 end
