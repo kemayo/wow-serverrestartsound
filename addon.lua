1b8b43a8cc844298cd644ff9063bc6b457ad8c63
David Lynch <kemayo@gmail.com>
2019-09-14 20:00:56 -0500

    Fix for 80200

--- addon.lua
+++ addon.lua
@@ -18,29 +18,31 @@ function ns:UnregisterEvent(...) for i=1,select("#", ...) do f:UnregisterEvent((
 --8/2 08:03:29.127  [SERVER] This realm is scheduled for a rolling restart. Thank you for your patience and understanding.
 --8/2 08:03:35.504  [SERVER] Restart in 10:00
 --8/2 08:03:35.504  [SERVER] Shutdown in 10:00
+--/script ServerRestart:CHAT_MSG_SYSTEM("CHAT_MSG_SYSTEM", IDLE_MESSAGE)
 local realmTimer = 0
 function ns:CHAT_MSG_SYSTEM(event, msg)
 	if msg == IDLE_MESSAGE then
-		PlaySoundFile("Sound\\Creature\\AlgalonTheObserver\\UR_Algalon_Berzerk01.wav", "Master")
+		PlaySoundFile(543569, "Master") -- Sound\\Creature\\AlgalonTheObserver\\UR_Algalon_Berzerk01.ogg
+		return
 	end
 	if not msg:find("[SERVER]") then return end
 	if GetTime() - realmTimer < 2 then return end
 	realmTimer = GetTime()
 	if msg:find("realm") and (msg:find("restart") and not msg:find("Cross") or msg:find("maintenance")) then
 		--Message contains both "realm" and "restart" or "realm" and "maintenance" in same sentence it's probably the right message
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSDStart01.wav", "Master")
+		PlaySoundFile(544153, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSDStart01.ogg
 	elseif msg:find(" 10:00") then
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD10.wav", "Master")
+		PlaySoundFile(544184, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD10.ogg
 	elseif msg:find(" 5:00") then
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD05.wav", "Master")
+		PlaySoundFile(544189, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD05.ogg
 	elseif msg:find(" 3:00") then
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD03.wav", "Master")
+		PlaySoundFile(544186, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD03.ogg
 	elseif msg:find(" 2:00") then
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD02.wav", "Master")
+		PlaySoundFile(544152, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD02.ogg
 	elseif msg:find(" 1:00") then
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD01.wav", "Master")
+		PlaySoundFile(544204, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD01.ogg
 	elseif msg:find(" 0:15") then
-		PlaySoundFile("Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD00.wav", "Master")
+		PlaySoundFile(544176, "Master") -- Sound\\Creature\\ArchivumSystem\\UR_Archivum_MimironSD00.ogg
 	end
 end
 ns:RegisterEvent("CHAT_MSG_SYSTEM")
